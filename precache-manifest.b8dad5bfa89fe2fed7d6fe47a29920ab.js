29f04f73226d42624fad5dbff0176cd1bc279189
bt-ausbildung <ng-ausbildung@gmail.com>
2022-08-11 12:20:37 +0200

    Updates

---
+++ precache-manifest.b8dad5bfa89fe2fed7d6fe47a29920ab.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = [
+  {
+    "revision": "4e66e94b8dd779dd302c",
+    "url": "/player-redux/static/js/runtime~main.4e66e94b.js"
+  },
+  {
+    "revision": "97c7ec8599d67b031088",
+    "url": "/player-redux/static/js/main.97c7ec85.chunk.js"
+  },
+  {
+    "revision": "0dab129ee8635765cb43",
+    "url": "/player-redux/static/js/1.0dab129e.chunk.js"
+  },
+  {
+    "revision": "97c7ec8599d67b031088",
+    "url": "/player-redux/static/css/main.e4874026.chunk.css"
+  },
+  {
+    "revision": "a0dbbe7ab4f41110048a511a88a25d89",
+    "url": "/player-redux/index.html"
+  }
+];
